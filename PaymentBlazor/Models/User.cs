27b1b0b2171c858b66ca048a76ea5c5648a32602
mion24 <guilherme.milhomemf@gmail.com>
2023-07-18 22:32:09 -0400

    Front end

---
+++ PaymentBlazor/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace PaymentBlazor.Models
+{
+    public class User
+    {
+        public string Email{ get; set; }
+        public string Password { get; set; }
+    }
+}
